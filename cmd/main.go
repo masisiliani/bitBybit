dc4bcafb044f93cfbebed6292a04f18a858568fd
msisiliani <marcela.silva@resultadosdigitais.com.br>
2019-09-09 13:51:52 -0300

    Adjusts repository

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+
+	sqlDb "github.com/masisiliani/bitBybit/db"
+	"github.com/masisiliani/bitBybit/pkg/user"
 )
 
 func main() {
-	fmt.Println("Hello bit.By.bi")
+	fmt.Println("Hello bit.By.bit")
 
-	
+	err := sqlDb.Connect_SQL()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	userRepo := user.NewSqlServerRepository(sqlDb.DB)
+	userService := user.NewService(userRepo)
+
+	user, err := userService.Find(1)
+	fmt.Println(user.Login)
+
 }
