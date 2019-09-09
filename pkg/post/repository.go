12de75320f0916e9fa49829c73436679f24a5166
vitoria <vitoria.mdrio@gmail.com>
2019-09-09 17:24:14 -0300

    Add insecure database calls

--- pkg/post/repository.go
+++ pkg/post/repository.go
@@ -1,1 +1,136 @@
 package post
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+)
+
+//SQLServerRepository mongodb repo
+type SQLServerRepository struct {
+	DB *sql.DB
+}
+
+//NewSqlServerRepository create new repository
+func NewSqlServerRepository(db *sql.DB) *SQLServerRepository {
+	return &SQLServerRepository{
+		DB: db,
+	}
+}
+
+func (r *SQLServerRepository) FindPosts(username string) ([]Post, error) {
+	rows, err := r.DB.Query(`SELECT
+							ID, 
+							Description, 
+							User
+						FROM Post 
+						WHERE User = ` + username)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer rows.Close()
+
+	var posts []Post
+	var post Post
+
+	for rows.Next() {
+
+		err = rows.Scan(
+			&post.ID,
+			&post.Description,
+			&post.User,
+		)
+
+		if err != nil {
+			fmt.Println(err)
+		}else{
+			posts = append(posts, post)
+		}
+
+	}
+
+	return posts, err
+}
+
+
+func (r *SQLServerRepository) FindPost(ID int) (Post, error) {
+	rows, err := r.DB.Query(`SELECT
+							ID, 
+							Description, 
+							User
+						FROM Post 
+						WHERE ID = ` + strconv.Itoa(int(ID)))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer rows.Close()
+
+	var post Post
+
+	for rows.Next() {
+
+		err = rows.Scan(
+			&post.ID,
+			&post.Description,
+			&post.User,
+		)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+
+	return post, err
+}
+
+
+func (r *SQLServerRepository) InsertPost(username string, description string) (error) {
+	rows, err := r.DB.Query(`INSERT INTO
+							Posts (Description, User)
+							VALUES
+							(` + description + "," + username + ")")
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	rows.Close()
+	return  nil
+}
+
+
+func (r *SQLServerRepository) DeletePost(ID int) (error) {
+	rows, err := r.DB.Query(`DELETE FROM
+							Posts
+							VALUES
+							WHERE
+							ID = ` + strconv.Itoa(ID))
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	rows.Close()
+	return  nil
+}
+
+func (r *SQLServerRepository) UpdatePost(ID int, description string) (error) {
+	rows, err := r.DB.Query(`UPDATE
+							Posts
+							SET
+							Description = ` + description + 
+							` WHERE
+							ID = ` + strconv.Itoa(ID))
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	rows.Close()
+	return  nil
+}
--- pkg/user/entity.go
+++ pkg/user/entity.go
@@ -1,7 +1,6 @@
 package user
 
 type User struct {
-	ID       int
 	Login    string
 	Password string
 }
