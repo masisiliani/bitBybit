78569dca0152a3569a92cc5540484ba9e1f538a9
msisiliani <marcela.silva@resultadosdigitais.com.br>
2019-09-10 20:15:09 -0300

    Dockerfile

--- db/init.go
+++ db/init.go
@@ -12,19 +12,19 @@ func InitDatabase() error {
 	}
 
 	defer DB.Close()
-	if err := createPostsTable(); err != nil{
+	if err := createPostsTable(); err != nil {
 		return err
 	}
-	if err := createUsersTable(); err != nil{
+	if err := createUsersTable(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func createUsersTable() error{
-	query := fmt.Sprint(`CREATE TABLE Users 
-						(Username varchar(26), 
-						Password varchar(80), 
+func createUsersTable() error {
+	query := fmt.Sprint(`CREATE TABLE Users
+						(Username varchar(26),
+						Password varchar(80),
 						PRIMARY KEY (Username))`)
 	_, err := DB.Exec(query)
 	if err != nil {
@@ -34,11 +34,12 @@ func createUsersTable() error{
 	return nil
 }
 
-func createPostsTable() error{
-	query := fmt.Sprint(`CREATE TABLE Posts 
-						(ID int NOT NULL AUTO_INCREMENT, 
-						Description varchar(200), 
-						Username varchar(26), 
+func createPostsTable() error {
+	query := fmt.Sprint(`CREATE TABLE Posts
+						(ID int NOT NULL AUTO_INCREMENT,
+						Description varchar(200),
+						Username varchar(26),
+						Date varchar(20),
 						PRIMARY KEY (ID))`)
 	_, err := DB.Exec(query)
 	if err != nil {
